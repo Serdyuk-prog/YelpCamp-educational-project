1bc4b420a44ebcfa567f9b7165bbcf90223e9a53
Serdyuk-prog <tony.not.gloomy@gmail.com>
2022-02-08 19:13:15 +0700

    progress

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
@@ -19,6 +20,7 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.engine("ejs", ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
@@ -53,14 +55,16 @@ app.get("/campgrounds/:id/edit", async (req, res) => {
 
 app.put("/campgrounds/:id", async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const campground = await Campground.findByIdAndUpdate(id, {
+        ...req.body.campground,
+    });
     res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.delete("/campgrounds/:id", async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
+    res.redirect("/campgrounds");
 });
 
 app.listen(3000, () => {
--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam provident sequi nihila voluptates similique saepe nostrum deserunt eaque autem dignissimos dolor, velit nesciunt laborum, deleniti sed aspernatur enim. Dolores.",
+            price, 
         });
         await camp.save();
     }
